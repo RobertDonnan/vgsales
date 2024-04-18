62f2cb512cffe03d2abae332de239fa32c971445
robertdonnan38@yahoo.com <robertdonnan38@yahoo.com>
2024-04-18 12:50:18 +0000

    mounting dataset from path file and displaying results

--- VGSales Notebook.py
+++ VGSales Notebook.py
@@ -1,7 +1,23 @@
 # Databricks notebook source
-# DBTITLE 1,Mount Dataset 
+# DBTITLE 1,Mount games Dataset from storage container 
 dbutils.fs.mount(
-    source='wasbs://project-movies-data@moviesdatasa.blob.core.windows.net',
-    mount_point='/mnt/project-movies-data',
-    extra_configs = {'fs.azure.account.key.moviesdatasa.blob.core.windows.net': dbutils.secrets.get('projectmoviesscope', 'storageAccountKey')}
+    source='wasbs://games@moviesdataset.blob.core.windows.net',
+    mount_point='/mnt/games',
+    extra_configs = {'fs.azure.account.key.moviesdataset.blob.core.windows.net': dbutils.secrets.get('projectmoviesscope', 'storageAccountKey')}
+)
 
+# COMMAND ----------
+
+# DBTITLE 1,Display path information 
+# MAGIC %fs
+# MAGIC ls "/mnt/games"
+
+# COMMAND ----------
+
+# DBTITLE 1,Reads file format
+games = spark.read.format("csv").option("header","true").load("/mnt/games/raw-data/vgsales.csv")
+
+# COMMAND ----------
+
+# DBTITLE 1,Displays first 20 results 
+games.show()
