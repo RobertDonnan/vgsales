1d779665cae77bc3a159b1de7d2812385f02c0da
robertdonnan38@yahoo.com <robertdonnan38@yahoo.com>
2024-04-18 13:35:26 +0000

    Viewing and transforming data

--- VGSales Notebook.py
+++ VGSales Notebook.py
@@ -21,3 +21,85 @@ games = spark.read.format("csv").option("header","true").load("/mnt/games/raw-da
 
 # DBTITLE 1,Displays first 20 results 
 games.show()
+
+# COMMAND ----------
+
+# DBTITLE 1,Print Schema to show data type 
+games.printSchema()
+
+# COMMAND ----------
+
+# DBTITLE 1,Import functions and types to convert data types 
+from pyspark.sql.functions import col
+from pyspark.sql.types import IntegerType, DoubleType, BooleanType, DataType
+
+# COMMAND ----------
+
+# DBTITLE 1,Convert Global Sales to Int
+games = games.withColumn("Rank", col("Rank").cast(IntegerType()))
+
+# COMMAND ----------
+
+# DBTITLE 1,Reprint schema to ensure Global Sales has changed to Int 
+games.printSchema()
+
+# COMMAND ----------
+
+# DBTITLE 1,Show results again to view change 
+games.show()
+
+# COMMAND ----------
+
+# DBTITLE 1,Converts Year to int 
+games = games.withColumn("Year", col("Year").cast(IntegerType()))
+
+# COMMAND ----------
+
+# DBTITLE 1,Cast global Sales to double 
+#converted to double to avoid numbers rounding down if an int was used 
+games = games.withColumn("Global_Sales", col("Global_Sales").cast(DoubleType()))
+
+# COMMAND ----------
+
+# DBTITLE 1,Displaying table to ensure numbers in global sales remain the same 
+games.show()
+
+# COMMAND ----------
+
+# DBTITLE 1,Cast North America Sales to double 
+games = games.withColumn("NA_Sales", col("NA_Sales").cast(DoubleType()))
+
+# COMMAND ----------
+
+# DBTITLE 1,Cast EU Sales to double 
+games = games.withColumn("EU_Sales", col("EU_Sales").cast(DoubleType()))
+
+# COMMAND ----------
+
+# DBTITLE 1,Cast Japan Sales to double 
+games = games.withColumn("JP_Sales", col("JP_Sales").cast(DoubleType()))
+
+# COMMAND ----------
+
+# DBTITLE 1,Cast Japan Sales to double
+games = games.withColumn("Other_Sales", col("Other_Sales").cast(DoubleType()))
+
+# COMMAND ----------
+
+# DBTITLE 1,Display table to ensure numbers have changed to double
+games.show()
+
+# COMMAND ----------
+
+# DBTITLE 1,Shows Schema to ensure correct and all changes have been made 
+games.printSchema()
+
+# COMMAND ----------
+
+# DBTITLE 1,Creating new dataframe with updated column name 
+gamesNameChange = games.withColumnRenamed("JP_Sales", "Japan_Sales")
+
+# COMMAND ----------
+
+# DBTITLE 1,Showing new column name change
+gamesNameChange.show()
